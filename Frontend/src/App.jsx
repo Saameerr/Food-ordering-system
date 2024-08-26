f81ddfd80c4d8474132820655346a34fe60e0239
Saameerr <samirbhattarai1218@gmail.com>
2024-08-26 21:14:23 +0545

    login component created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './Components/Login'
 import './App.css'
 
 function App() {
@@ -8,9 +7,8 @@ function App() {
 
   return (
     <>
-        <h1> Hello this is a foodie website</h1>
-        <img src="logo.png" alt="" />
-        </>
+    <Login />
+    </>
   )
 }
 
