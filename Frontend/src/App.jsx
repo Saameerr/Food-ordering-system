30a4ab70196948fa226b65035fcbc463889a34f0
Saameerr <samirbhattarai1218@gmail.com>
2024-08-27 10:19:43 +0545

    chnages in error components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home"
+import Error from './Components/Error';
 import Login from './Components/Login';
 import Register from './Components/Register';
+
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/*" element={<Error />} />
       </Routes>
--- Frontend/src/Components/Error.jsx
+++ Frontend/src/Components/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
   return (
     <div>
-      <h1>page not found</h1>
+      <img src="error1.png" alt=""  style={{height:"40rem", width:"40rem", marginLeft:"25rem"}}/>
     </div>
   )
 }
