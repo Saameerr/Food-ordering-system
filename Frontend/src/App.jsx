8bb09fa9c1e070533fe9fd4ae059fbeb87b3ea7f
Saameerr <samirbhattarai1218@gmail.com>
2024-09-13 08:51:32 +0545

    first commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Error from './Signup/Error';
-import Login from './Signup/Login';
-import Forgot_password from './Signup/Forgot_password';
-import Register from './Signup/Register';
-import Dashboard from './Home Page/Dashboard';
 
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot_password" element={<Forgot_password />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+   <h1>hello</h1>
   )
 }
 
