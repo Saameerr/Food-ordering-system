bf3e5b594927be380c5015dbeda6c44b722c6aed
Saameerr <samirbhattarai1218@gmail.com>
2024-09-21 12:20:06 +0545

    exploreMenu component created

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Carousel from '../../components/Carousel/Carousel'
 import AppDownload from '../../AppDownload/AppDownload'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+  const[category, setCategory]= useState("All")
   return (
     <div>
       <Carousel/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
       <AppDownload/>
+      
     </div>
   )
 }
