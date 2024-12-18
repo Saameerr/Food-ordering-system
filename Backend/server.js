e93a137c94c47c033bc94c7bade9d77d1ca31490
Saameerr <samirbhattarai1218@gmail.com>
2024-12-18 21:48:59 +0545

    shopping cart functionality

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
 
 
 //app config
@@ -14,6 +15,7 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 
+
 //db connection
 connectDB();
 
@@ -21,6 +23,7 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user", userRouter)
+app.use("/api/cart",cartRouter)
 
 
 
