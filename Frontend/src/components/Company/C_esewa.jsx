4ef7105e62215717063edeba4cd86247a46755ca
Saameerr <samirbhattarai1218@gmail.com>
2025-01-22 13:19:24 +0545

    chnages in footer and app.jsx

---
+++ Frontend/src/components/Company/C_esewa.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const C_esewa = () => {
+  return (
+    <div className='company'>
+    <div className="content">
+    <h2>eSewa Payment at KhajaBhayo!</h2>
+    <p>At KhajaBhayo, we offer a secure and convenient payment option through eSewa, Nepal's leading digital wallet. With eSewa, you can easily pay for your orders using your eSewa account balance. This fast and reliable payment method ensures a seamless experience from order to delivery. For more details, you can visit the official <a href="https://esewa.com.np/#/home" target="_blank" style={{color:"blue"}}>eSewa Documentation</a>  to learn how to set up and use your eSewa account..</p>
+    </div>
+      
+    
+  </div>
+  )
+}
+
+export default C_esewa
